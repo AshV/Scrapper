e83e5782703fe486fa0af3b80ecee63fdb4a6bbe
AshV <ashish6318@gmail.com>
2017-07-26 09:00:27 +0530

    Imported HTML Agility

--- Scrapper/Program.cs
+++ Scrapper/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,11 @@ namespace Scrapper
     {
         static void Main(string[] args)
         {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = new HtmlDocument();
+            document.Load("List.html");
+            var bookBoxes = document.DocumentNode.SelectNodes("//*[@class='col-lg-4 col-md-4 col-sm-4 col-xs-12 list-details']");
+            //  .Attributes["class"];//.Value.Contains("list-details");
         }
     }
 }
